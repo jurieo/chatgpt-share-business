5eaddaf29c5f865158be36600e2490a695c68e95
jurieo <jurieo@jurieo.com>
2024-03-07 14:28:49 +0800

    feat: update

--- quick-install.sh
+++ quick-install.sh
@@ -3,21 +3,19 @@ set -e
 
 ## 克隆仓库到本地
 echo "clone repository..."
-git clone -b deploy --depth=1 https://github.com/jurieo/chatgpt-share-web.git chatgpt-share-web
+git clone -b deploy-free --depth=1 https://github.com/jurieo/chatgpt-share-web.git chatgpt-share-web
 
 ## 进入目录
 cd chatgpt-share-web
 
 chmod +x ./deploy.sh
 
-# docker compose pull
-# docker compose up -d --remove-orphans
+docker compose pull
+docker compose up -d --remove-orphans
 
 ## 提示信息
-echo "请进入chatgpt-share-web目录 (cd chatgpt-share-web),修改config.yaml文件中的重要信息,然后执行以下命令"
-echo "./deploy.sh"
-echo "服务启动成功后，请访问://localhost:38300"
-echo "管理员后台地址:http://localhost:38300/xyhelper"
+echo "服务启动成功，请访问 http://localhost:8300"
+echo "管理员后台地址 http://localhost:8300/xyhelper"
 echo "管理员账号: admin"
 echo "管理员密码: 123456"
 echo "请及时修改管理员密码"
