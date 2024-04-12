bc33471a7c5d380f5dfe2d525286343fbbfe9681
jurieo <jurieo@jurieo.com>
2024-04-12 11:17:23 +0800

    docs: update

--- quick-install.sh
+++ quick-install.sh
@@ -5,6 +5,8 @@ set -e
 echo "clone repository..."
 git clone -b deploy-free --depth=1 https://github.com/jurieo/chatgpt-share-web.git chatgpt-share-web
 
+local_ipv4=$(curl -s4m8 http://ip.sb)
+
 ## 进入目录
 cd chatgpt-share-web
 
@@ -14,8 +16,12 @@ docker compose pull
 docker compose up -d --remove-orphans
 
 ## 提示信息
-echo "服务启动成功，请访问 http://localhost:8300"
-echo "管理员后台地址 http://localhost:8300/xyhelper"
-echo "管理员账号: admin"
-echo "管理员密码: 123456"
-echo "请及时修改管理员密码"
+echo "=================感谢您的耐心等待，部署已完成！=================== "
+echo ""
+echo "请访问: http://${local_ipv4}:8300 开始使用！"
+echo "管理员后台地址: http://${local_ipv4}:8300/xyhelper"
+echo "后台管理员账号/密码: 【admin/123456】，请及时修改管理员密码"
+echo "通过反代服务器的8300端口，即可使用域名访问您的服务。"
+echo "有任何问题请加入TG群: https://t.me/chatgpt_share_web"
+echo ""
+echo "============================================================= "
